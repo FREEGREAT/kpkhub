63f1f1ffaa35beea2cf24cd86aa4a5dae24e57b2
DiSfiAl <pashakcnk@gmail.com>
2023-04-07 12:16:39 +0300

    Schedule for p41 and p42 added

---
+++ app/src/main/java/com/example/kpkhub/P41Activity.kt
@@ -0,0 +1,11 @@
+package com.example.kpkhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class P41Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p41)
+    }
+}
